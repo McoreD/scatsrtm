2c42d22abfc122a4a15c25f1322a42137f5eef04
mike.delpach@mainroads.wa.gov.au <mike.delpach@mainroads.wa.gov.au@979d7102-210a-11df-a5a4-5bcf8f6a2e7e>
2010-04-01 03:07:00 +0000

    added SM Exporter 2.0 (rewrite fo SMAnalyzer 1.0 which was written in VB.net)

---
+++ trunk/SMExporter/SMExporter/Helpers/SMParser.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace SMExporter
+{
+
+    public class SMParser
+    {
+        private string mFilePath = string.Empty;
+        private string mSMtitle = string.Empty;
+        private int MaxValue { get; set; }
+        private int CurrValue { get; set; }
+
+        public SMParser(string fp)
+        {
+            mFilePath = fp;
+        }
+
+        public List<Reading> AnalyzeFile()
+        {
+
+            List<Reading> myListReadings = new List<Reading>();
+            StreamReader sr = new StreamReader(mFilePath);
+            string currLine = sr.ReadLine();
+            mSMtitle = currLine;
+
+            // int countReadings = 0;
+            string prevLine = "";
+            Reading currRead = default(Reading);
+            int readID = 0;
+
+            using (StreamReader tempSr = new StreamReader(mFilePath))
+            {
+                Adapter.MaxValue = Regex.Split(tempSr.ReadToEnd(), Environment.NewLine).Length;
+                Adapter.CurrentValue = 0;
+            }
+
+            while (!sr.EndOfStream)
+            {
+
+                currLine = sr.ReadLine();
+
+                Adapter.CurrentValue += 1;
+                CurrValue += 1;
+
+                if (currLine != null && currLine.Length > 0)
+                {
+
+                    currRead = new Reading();
+
+                    // this sub ensures that headings are read only if there are 
+                    // active intersection data
+
+                    if (currLine.Substring(0, 4) == " Int")
+                    {
+                        // break curr line 
+                        string fixedLine = prevLine;
+                        fixedLine = fixedLine.Replace(" + ", " ");
+                        fixedLine = fixedLine.Replace(" - ", " ");
+                        fixedLine = fixedLine.Replace("'", " ");
+                        fixedLine = fixedLine.Replace("''", " ");
+                        fixedLine = fixedLine.Replace("  ", " ").Replace("  ", " ");
+
+                        string[] arrLine = Regex.Split(fixedLine, " ");
+                        currRead.DayRead = arrLine[0].ToString();
+                        currRead.DateRead = arrLine[1].ToString();
+                        currRead.Time = arrLine[2].ToString();
+
+                        currRead.PlansActive = arrLine[6];
+                        double plansVoted = 0.0;
+                        Double.TryParse(arrLine[8], out plansVoted);
+                        currRead.PlansVoted = plansVoted;
+
+                        string cl = arrLine[10].Trim();
+                        int clN = 0;
+                        int clA = 0;
+
+                        string[] arr = cl.Split('+'); // Regex.Split(cl, "+");
+                        int.TryParse(Regex.Split(arr[0], "-")[0], out clN);
+                        clA = clN;
+
+                        if (arr.Length > 1)
+                        {
+                            int temp = 0;
+                            int.TryParse(arr[1], out temp);
+                            clA = clN + temp;
+                        }
+                        else
+                        {
+                            arr = Regex.Split(cl, "-");
+                            if (arr.Length > 1)
+                            {
+                                int temp = 0;
+                                int.TryParse(arr[1], out temp);
+                                clA = clN - temp;
+                            }
+                            else
+                            {
+                                int offset = 0;
+                                if (Program.IsNumeric(arrLine[11].Trim()))
+                                {
+                                    int.TryParse(arrLine[11], out offset);
+                                    clA = clN + offset;
+                                }
+                            }
+                        }
+
+                        currRead.CycleLengthNominal = clN;
+                        currRead.CycleLengthActual = clA;
+
+                        // this has to be done because when 70+18 for example, happens
+                        // we lose one split string
+
+                        if (Program.IsNumeric(arrLine[13]))
+                        {
+                            int temp = 0;
+                            int.TryParse(arrLine[13], out temp);
+                            currRead.CycleLengthRequired = temp;
+                        }
+                        else
+                        {
+                            int temp = 0;
+                            int.TryParse(arrLine[12], out temp);
+                            currRead.CycleLengthRequired = temp;
+                        }
+
+
+                        if (Program.IsNumeric(arrLine[15]))
+                        {
+                            int temp = 0;
+                            int.TryParse(arrLine[15], out temp);
+                            currRead.CycleLengthSA = temp;
+                        }
+                        else
+                        {
+                            int temp = 0;
+                            int.TryParse(arrLine[14], out temp);
+                            currRead.CycleLengthSA = temp;
+                        }
+
+                        //Console.WriteLine (cl.ToString & " is " & clN.ToString & " and " & cla.ToString)
+
+                        myListReadings.Add(currRead);
+
+                        readID += 1;
+                    }
+                    else if (currLine.Substring(0, 2) == "  ")
+                    {
+
+                        // count number of lanes
+                        currLine = currLine.Trim();
+                        currLine = currLine.Replace("'", "");
+                        currLine = currLine.Replace(">", "!");
+                        currLine = currLine.Replace("^", "");
+                        currLine = currLine.Replace("  ", " ");
+                        currLine = currLine.Replace("  ", " ");
+                        currLine = currLine.Replace("  ", " ");
+
+                        string[] fixValues = Regex.Split(currLine, "!");
+                        Approach approach = new Approach();
+
+                        try
+                        {
+                            int temp = 0;
+                            int.TryParse(Regex.Split(fixValues[0], " ")[0], out temp);
+                            approach.Intersection = temp;
+                            int temp1, temp2 = 0;
+                            int.TryParse(Regex.Split(fixValues[0], " ")[1], out temp1);
+                            int.TryParse(Regex.Split(fixValues[0], " ")[2], out temp2);
+                            approach.SAorLK = Regex.Split(fixValues[0], " ")[1] + Regex.Split(fixValues[0], " ")[2];
+                            int temp3, temp4 = 0;
+                            int.TryParse(Regex.Split(fixValues[0], " ")[3], out temp3);
+                            approach.PhaseId = temp3;
+                            int.TryParse(Regex.Split(fixValues[0], " ")[4], out temp4);
+                            approach.PhaseTime = temp4;
+                            if (Program.IsNumeric(fixValues[fixValues.Length - 1]))
+                            {
+                                int ads = 0;
+                                int.TryParse(fixValues[fixValues.Length - 1].Trim(), out ads);
+                                approach.ADS = ads;
+                            }
+                            else
+                            {
+                                approach.ADS = 0;
+
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(CurrValue + " " + currLine);
+                            throw ex;
+                        }
+
+
+                        int numLanes = fixValues.Length - 2;
+
+                        for (int i = 1; i <= numLanes; i++)
+                        {
+
+                            if (approach.PhaseTime > 0)
+                            {
+
+                                Lane lane = new Lane();
+
+                                lane.LaneId = i;
+
+                                int ds = 0;
+                                int.TryParse(Regex.Split(fixValues[i].Trim(), " ")[0], out ds);
+                                lane.DS = ds;
+
+                                if (fixValues[i].Trim().Length > 2)
+                                {
+                                    int vo = 0;
+                                    int.TryParse(Regex.Split(fixValues[i].Trim(), " ")[1], out vo);
+                                    lane.VO = vo;
+                                }
+                                else
+                                {
+                                    lane.VO = 0;
+                                }
+
+                                if (fixValues[i].Trim().Length > 3)
+                                {
+                                    int vk = 0;
+                                    int.TryParse(Regex.Split(fixValues[i].Trim(), " ")[2], out vk);
+                                    lane.VK = vk;
+                                }
+                                else
+                                {
+                                    lane.VK = 0;
+                                }
+
+                                if (approach.PhaseTime > 0 && lane.DS > 0)
+                                {
+                                    lane.MaxFlow = (3600 * lane.VK) / (approach.PhaseTime * (lane.DS / 100.0));
+                                }
+
+                                if (myListReadings[readID - 1].CycleLengthActual > 0)
+                                {
+                                    lane.Capacity = lane.MaxFlow * approach.PhaseTime / myListReadings[readID - 1].CycleLengthActual;
+                                }
+
+                                if (lane.DS > 0 | lane.VO > 0 | lane.VK > 0)
+                                {
+                                    approach.Lanes.Add(lane);
+
+                                }
+
+                            }
+                        }
+
+
+                        myListReadings[readID - 1].Approaches.Add(approach);
+                    }
+                    else if (fIsPhaseDataLine(currLine))
+                    {
+
+                        string[] phases = Regex.Split(Regex.Replace(currLine, "  ", " "), " "); 
+                        if (phases.Length > 0)
+                        {
+                            myListReadings[readID - 1].PhaseA = phases[0].Replace("A=", "").Trim();
+                        }
+                        if (phases.Length > 1)
+                        {
+                            myListReadings[readID - 1].PhaseB = phases[1].Replace("B=", "").Trim();
+                        }
+                        if (phases.Length > 2)
+                        {
+                            myListReadings[readID - 1].PhaseC = phases[2].Replace("C=", "").Trim();
+                        }
+                        if (phases.Length > 3)
+                        {
+                            myListReadings[readID - 1].PhaseD = phases[3].Replace("D=", "").Trim();
+                        }
+                        if (phases.Length > 4)
+                        {
+                            myListReadings[readID - 1].PhaseE = phases[4].Replace("E=", "").Trim();
+                        }
+                        if (phases.Length > 5)
+                        {
+                            myListReadings[readID - 1].PhaseF = phases[5].Replace("F=", "").Trim();
+                        }
+                        if (phases.Length > 6)
+                        {
+                            myListReadings[readID - 1].PhaseG = phases[6].Replace("G=", "").Trim();
+                        }
+                    }
+
+
+                }
+                // currLine IsNot Nothing
+
+
+                prevLine = currLine;
+            }
+
+            sr.Close();
+
+            return myListReadings;
+        }
+
+        private bool fIsPhaseDataLine(string line)
+        {
+            return line.Substring(0, 2) == "A=" || line.Substring(0, 2) == "B=" || line.Substring(0, 2) == "C=" || line.Substring(0, 2) == "D=" || line.Substring(0, 2) == "E=";
+        }
+    }
+}
