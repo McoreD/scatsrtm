814211495bbdb331afba6d2f16382dc206cc9e67
mike.delpach@mainroads.wa.gov.au <mike.delpach@mainroads.wa.gov.au@979d7102-210a-11df-a5a4-5bcf8f6a2e7e>
2010-04-09 03:06:44 +0000

    SMExporter updates

--- HistoryFileDumper/MainForm.Designer.cs
+++ HistoryFileDumper/MainForm.Designer.cs
@@ -36,121 +36,121 @@ namespace HistoryFileDumper
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.btnSync = new System.Windows.Forms.Button();
-			this.label1 = new System.Windows.Forms.Label();
-			this.txtSourceDir = new System.Windows.Forms.TextBox();
-			this.label2 = new System.Windows.Forms.Label();
-			this.txtDestDir = new System.Windows.Forms.TextBox();
-			this.nudFreq = new System.Windows.Forms.NumericUpDown();
-			this.label3 = new System.Windows.Forms.Label();
-			this.tmrApp = new System.Windows.Forms.Timer(this.components);
-			this.bwApp = new System.ComponentModel.BackgroundWorker();
-			((System.ComponentModel.ISupportInitialize)(this.nudFreq)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// btnSync
-			// 
-			this.btnSync.Location = new System.Drawing.Point(400, 128);
-			this.btnSync.Name = "btnSync";
-			this.btnSync.Size = new System.Drawing.Size(75, 23);
-			this.btnSync.TabIndex = 0;
-			this.btnSync.Text = "&Start";
-			this.btnSync.UseVisualStyleBackColor = true;
-			this.btnSync.Click += new System.EventHandler(this.BtnSyncClick);
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(40, 16);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(44, 13);
-			this.label1.TabIndex = 1;
-			this.label1.Text = "Source:";
-			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-			// 
-			// txtSourceDir
-			// 
-			this.txtSourceDir.Location = new System.Drawing.Point(112, 16);
-			this.txtSourceDir.Name = "txtSourceDir";
-			this.txtSourceDir.Size = new System.Drawing.Size(363, 20);
-			this.txtSourceDir.TabIndex = 0;
-			this.txtSourceDir.Text = "\\\\dactst30\\SCATSData\\History";
-			// 
-			// label2
-			// 
-			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(24, 56);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(63, 13);
-			this.label2.TabIndex = 1;
-			this.label2.Text = "Destination:";
-			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-			// 
-			// txtDestDir
-			// 
-			this.txtDestDir.Location = new System.Drawing.Point(112, 48);
-			this.txtDestDir.Name = "txtDestDir";
-			this.txtDestDir.Size = new System.Drawing.Size(363, 20);
-			this.txtDestDir.TabIndex = 0;
-			this.txtDestDir.Text = "\\\\tocuat20\\SCATSData\\History";
-			// 
-			// nudFreq
-			// 
-			this.nudFreq.Location = new System.Drawing.Point(112, 80);
-			this.nudFreq.Minimum = new decimal(new int[] {
-									1,
-									0,
-									0,
-									0});
-			this.nudFreq.Name = "nudFreq";
-			this.nudFreq.Size = new System.Drawing.Size(120, 20);
-			this.nudFreq.TabIndex = 3;
-			this.nudFreq.Value = new decimal(new int[] {
-									3,
-									0,
-									0,
-									0});
-			this.nudFreq.ValueChanged += new System.EventHandler(this.nudFreqValueChanged);
-			// 
-			// label3
-			// 
-			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(24, 88);
-			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(71, 13);
-			this.label3.TabIndex = 4;
-			this.label3.Text = "Frequency (s)";
-			this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-			// 
-			// tmrApp
-			// 
-			this.tmrApp.Interval = 3000;
-			this.tmrApp.Tick += new System.EventHandler(this.TmrAppTick);
-			// 
-			// bwApp
-			// 
-			this.bwApp.WorkerReportsProgress = true;
-			this.bwApp.WorkerSupportsCancellation = true;
-			this.bwApp.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BwAppDoWork);
-			// 
-			// MainForm
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(494, 166);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.txtDestDir);
-			this.Controls.Add(this.txtSourceDir);
-			this.Controls.Add(this.label2);
-			this.Controls.Add(this.label3);
-			this.Controls.Add(this.nudFreq);
-			this.Controls.Add(this.btnSync);
-			this.Name = "MainForm";
-			this.Text = "HistoryFileDumper";
-			((System.ComponentModel.ISupportInitialize)(this.nudFreq)).EndInit();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.components = new System.ComponentModel.Container();
+            this.btnSync = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDestDir = new System.Windows.Forms.TextBox();
+            this.nudFreq = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tmrApp = new System.Windows.Forms.Timer(this.components);
+            this.bwApp = new System.ComponentModel.BackgroundWorker();
+            this.txtSourceDir = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudFreq)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSync
+            // 
+            this.btnSync.Location = new System.Drawing.Point(400, 128);
+            this.btnSync.Name = "btnSync";
+            this.btnSync.Size = new System.Drawing.Size(75, 23);
+            this.btnSync.TabIndex = 0;
+            this.btnSync.Text = "&Start";
+            this.btnSync.UseVisualStyleBackColor = true;
+            this.btnSync.Click += new System.EventHandler(this.BtnSyncClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Source:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Destination:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtDestDir
+            // 
+            this.txtDestDir.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::HistoryFileDumper.Properties.Settings.Default, "DestDir", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.txtDestDir.Location = new System.Drawing.Point(112, 48);
+            this.txtDestDir.Name = "txtDestDir";
+            this.txtDestDir.Size = new System.Drawing.Size(363, 20);
+            this.txtDestDir.TabIndex = 0;
+            this.txtDestDir.Text = global::HistoryFileDumper.Properties.Settings.Default.DestDir;
+            // 
+            // nudFreq
+            // 
+            this.nudFreq.DataBindings.Add(new System.Windows.Forms.Binding("Value", global::HistoryFileDumper.Properties.Settings.Default, "Freq", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.nudFreq.Location = new System.Drawing.Point(112, 80);
+            this.nudFreq.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudFreq.Name = "nudFreq";
+            this.nudFreq.Size = new System.Drawing.Size(120, 20);
+            this.nudFreq.TabIndex = 3;
+            this.nudFreq.Value = global::HistoryFileDumper.Properties.Settings.Default.Freq;
+            this.nudFreq.ValueChanged += new System.EventHandler(this.nudFreqValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Frequency (s)";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // tmrApp
+            // 
+            this.tmrApp.Interval = 3000;
+            this.tmrApp.Tick += new System.EventHandler(this.TmrAppTick);
+            // 
+            // bwApp
+            // 
+            this.bwApp.WorkerReportsProgress = true;
+            this.bwApp.WorkerSupportsCancellation = true;
+            this.bwApp.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BwAppDoWork);
+            // 
+            // txtSourceDir
+            // 
+            this.txtSourceDir.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::HistoryFileDumper.Properties.Settings.Default, "SourceDir", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.txtSourceDir.Location = new System.Drawing.Point(112, 16);
+            this.txtSourceDir.Name = "txtSourceDir";
+            this.txtSourceDir.Size = new System.Drawing.Size(363, 20);
+            this.txtSourceDir.TabIndex = 0;
+            this.txtSourceDir.Text = global::HistoryFileDumper.Properties.Settings.Default.SourceDir;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 166);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDestDir);
+            this.Controls.Add(this.txtSourceDir);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.nudFreq);
+            this.Controls.Add(this.btnSync);
+            this.Name = "MainForm";
+            this.Text = "HistoryFileDumper";
+            ((System.ComponentModel.ISupportInitialize)(this.nudFreq)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
 		}
 		private System.Windows.Forms.NumericUpDown nudFreq;
 		private System.ComponentModel.BackgroundWorker bwApp;
--- HistoryFileDumper/MainForm.cs
+++ HistoryFileDumper/MainForm.cs
@@ -14,62 +14,62 @@ using System.Windows.Forms;
 
 namespace HistoryFileDumper
 {
-	/// <summary>
-	/// Description of MainForm.
-	/// </summary>
-	public partial class MainForm : Form
-	{	
-		public MainForm()
-		{
-			//
-			// The InitializeComponent() call is required for Windows Forms designer support.
-			//
-			InitializeComponent();
-			
-			// Read  location \\dactst30\Program Files (x86)\SCATS6\Region\SCATSData\History
-			//  			  \\dactst30\SCATSData\History
-			// Write location \\tocuat20\SCATSData
-		}
-				
-		void BtnSyncClick(object sender, EventArgs e)
-		{
-			btnSync.Text = btnSync.Text == "&Start" ? "&Stop" : "&Start";				
-			tmrApp.Enabled = !tmrApp.Enabled;
-		}
-		
-		void CopyFiles()
-		{
-			string srcDir = txtSourceDir.Text; 
-			string destDir = txtDestDir.Text;			
-			if (Directory.Exists(srcDir) && Directory.Exists(destDir)) 
-			{
-				string[] files = Directory.GetFiles(srcDir, "*.hist", SearchOption.AllDirectories); 
-				foreach (string file in files)
-				{
-					string destFileName = Path.GetFileName(file);
-					string destFilePath = Path.Combine(destDir, destFileName);
-					File.Copy(file, destFilePath, true);
-				}
-			}
-		}
-		
-		void TmrAppTick(object sender, EventArgs e)
-		{
-			if (!bwApp.IsBusy) 
-			{
-				bwApp.RunWorkerAsync();
-			}
-		}
-		
-		void BwAppDoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
-		{
-			CopyFiles();							
-		}
-		
-		
-		void nudFreqValueChanged(object sender, EventArgs e)
-		{
-			tmrApp.Interval = 1000 * (int)nudFreq.Value;
-		}
-	}
+    /// <summary>
+    /// Description of MainForm.
+    /// </summary>
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            //
+            // The InitializeComponent() call is required for Windows Forms designer support.
+            //
+            InitializeComponent();
+
+            // Read  location \\dactst30\Program Files (x86)\SCATS6\Region\SCATSData\History
+            //  			  \\dactst30\SCATSData\History
+            // Write location \\tocuat20\SCATSData
+        }
+
+        void BtnSyncClick(object sender, EventArgs e)
+        {
+            btnSync.Text = btnSync.Text == "&Start" ? "&Stop" : "&Start";
+            tmrApp.Enabled = !tmrApp.Enabled;
+        }
+
+        void CopyFiles()
+        {
+            string srcDir = txtSourceDir.Text;
+            string destDir = txtDestDir.Text;
+            if (Directory.Exists(srcDir) && Directory.Exists(destDir))
+            {
+                string[] files = Directory.GetFiles(srcDir, "*.hist", SearchOption.AllDirectories);
+                foreach (string file in files)
+                {
+                    string destFileName = Path.GetFileName(file);
+                    string destFilePath = Path.Combine(destDir, destFileName);
+                    File.Copy(file, destFilePath, true);
+                }
+            }
+        }
+
+        void TmrAppTick(object sender, EventArgs e)
+        {
+            if (!bwApp.IsBusy)
+            {
+                bwApp.RunWorkerAsync();
+            }
+        }
+
+        void BwAppDoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            CopyFiles();
+        }
+
+
+        void nudFreqValueChanged(object sender, EventArgs e)
+        {
+            tmrApp.Interval = 1000 * (int)nudFreq.Value;
+        }
+    }
 }
