631d0260797bea586d3e74b432bbabab88ea3255
mike.delpach@mainroads.wa.gov.au <mike.delpach@mainroads.wa.gov.au@979d7102-210a-11df-a5a4-5bcf8f6a2e7e>
2010-04-09 04:08:38 +0000

    added missing files

---
+++ SCATS_Monitor/SCATS_Monitor/Helpers/Controller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SCATS_Monitor
+{
+    public class Controller
+    {
+        public string LM { get; set; }
+        public string IntersectionName { get; set; }
+        public string ControllerType { get; set; }
+        public string SCATSFault { get; set; }
+        public DateTime TimeOfFault { get; set; }
+        public string IPAddress { get; set; }
+        public DateTime CommsTimeChk { get; set; }
+        public string PossibleFault { get; set; }
+    }
+}
