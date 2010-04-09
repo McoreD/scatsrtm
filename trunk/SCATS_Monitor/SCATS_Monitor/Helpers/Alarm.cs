f6e3708e5bcb491542013e71349407221b6a6d3d
mike.delpach@mainroads.wa.gov.au <mike.delpach@mainroads.wa.gov.au@979d7102-210a-11df-a5a4-5bcf8f6a2e7e>
2010-04-09 04:08:38 +0000

    added missing files

---
+++ trunk/SCATS_Monitor/SCATS_Monitor/Helpers/Alarm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SCATS_Monitor
+{
+    public class Alarm
+    {
+        public string VMSName { get; set; }
+        public string Timestamp { get; set; }
+        public string AlarmType { get; set; }
+        public string AlarmDescription { get; set; }
+    }
+}
---
+++ trunk/SCATS_Monitor/SCATS_Monitor/WorkerAdmin.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: e80655
+ * Date: 9/04/2010
+ * Time: 11:26 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace SCATS_Monitor
+{
+    public class WorkerAdmin
+    {
+        public List<BackgroundWorker> Workers { get; set; }
+
+        public WorkerAdmin()
+        {
+            this.Workers = new List<BackgroundWorker>();
+        }
+    }
+}
